f179608563c1f47fe36f84d92c5ff6118359bd21
Oluwadebo <ogunweoluwadebo@gmail.com>
2023-03-08 12:15:04 +0100

    section home page done

--- src/component/Home.js
+++ src/component/Home.js
@@ -114,7 +114,7 @@ const Home = () => {
                             <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
                         <div className="carousel-inner">
-                            <div className="carousel-item active" data-bs-interval="1000">
+                            <div className="carousel-item active" data-bs-interval="100">
                                 <div className="styls1">
                                     <div className="carousel-caption">
                                         <div className="capt">
@@ -135,7 +135,7 @@ const Home = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="carousel-item" data-bs-interval="1000">
+                            <div className="carousel-item" data-bs-interval="100">
                                 <div className="styls2">
                                     <div className="carousel-caption">
                                         <div className="capt">
@@ -156,7 +156,7 @@ const Home = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="carousel-item" data-bs-interval="1000">
+                            <div className="carousel-item" data-bs-interval="100">
                                 <div className="styls3">
                                     <div className="carousel-caption">
                                         <div className="capt">
@@ -189,55 +189,51 @@ const Home = () => {
                         <div id="carouselExampleInterval" className="carousel slide my-5" data-bs-ride="carousel">
                             <div className="carousel-inner">
                                 <div className="carousel-item active" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgd" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3>Xavier</h3>
-                                        <p>“It’s my pleasure to work with Stack Delta. Even though the first opportunity they presented wasn’t the right fit for me, it did not stop them from finding me a position that suits my skill sets and qualifications. I was presented with an awesome opportunity. I appreciate their efforts in helping individuals like me find the right opportunity.”</p>
+                                    <div className="isd">
+                                        <div className="carousel-caption">
+                                            <h3 className='mb-3'>Xavier</h3>
+                                            <p>“It’s my pleasure to work with Stack Delta. Even though the first opportunity they presented wasn’t the right fit for me, it did not stop them from finding me a position that suits my skill sets and qualifications. I was presented with an awesome opportunity. I appreciate their efforts in helping individuals like me find the right opportunity.”</p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="carousel-item" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgd" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3> Paul</h3>
-                                        <p>
-                                            “The staff at Stack Delta is incredibly responsive, professional, and honest. They were highly focused on finding roles that fit my criteria and my experience. They consider your flexibility, compensation and your work ability. I recommend others who are seeking new opportunities to contact Stack Delta.”
-                                        </p>
+                                    <div className="isd">
+                                        <div className="carousel-caption">
+                                            <h3 className='mb-3'> Paul</h3>
+                                            <p>
+                                                “The staff at Stack Delta is incredibly responsive, professional, and honest. They were highly focused on finding roles that fit my criteria and my experience. They consider your flexibility, compensation and your work ability. I recommend others who are seeking new opportunities to contact Stack Delta.”
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="carousel-item" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgds" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3> Aman </h3>
-                                        <p>
-                                            “The team at Stack Delta has been amazing to work with. The team truly exceeds all expectations and provides qualified candidate profiles for our hiring needs. The type of candidates we receive from Stack Delta is remarkable - throughout the course of recent years we have had various promising workers for hire come through on impermanent bases who are currently a portion of our most grounded extremely durable increases to our staff. The staff is extremely watchful and brief with answering inquiries such as historical verifications, start dates, etc. The staff is proficient, productive, and dependable in offering extraordinary service.”
-                                        </p>
+                                    <div className="isd">
+                                        <div className="carousel-caption">
+                                            <h3 className='mb-3'> Arjun</h3>
+                                            <p>
+                                                “Stack Delta has been very helpful towards finding me a role which matched my experience, skills and guided me throughout the process. I felt very comfortable asking any questions I had, and they have been very positive and professional. I would recommend Stack Delta to anyone looking for new employment opportunities.”
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="carousel-item" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgd" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3> Arjun</h3>
-                                        <p>
-                                            “Stack Delta has been very helpful towards finding me a role which matched my experience, skills and guided me throughout the process. I felt very comfortable asking any questions I had, and they have been very positive and professional. I would recommend Stack Delta to anyone looking for new employment opportunities.”
-                                        </p>
+                                    <div className="isd">
+                                        <div className="carousel-caption">
+                                            <h3 className='mb-3'> Christina Cobb</h3>
+                                            <p>
+                                                “My involvement with working with Stack Delta has been exceptional. We are in consistent need of contractors for our clients. Some positions are temporary, and some are direct hire. Stack Delta submits resumes of professionals rapidly and efficiently. They give candidates who are proficient, talented, and meet the needs of various assignments. Stack Delta is proficient, productive, and dependable in offering remarkable candidates.”
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="carousel-item" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgde" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3> Christina Cobb </h3>
-                                        <p>
-                                            “My involvement with working with Stack Delta has been exceptional. We are in consistent need of contractors for our clients. Some positions are temporary, and some are direct hire. Stack Delta submits resumes of professionals rapidly and efficiently. They give candidates who are proficient, talented, and meet the needs of various assignments. Stack Delta is proficient, productive, and dependable in offering remarkable candidates.”
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="carousel-item" data-bs-interval="1000">
-                                    <img src={download1} className="img-fluid d-block mx-auto imgde" alt="..." />
-                                    <div className="carousel-caption see">
-                                        <h3> Michelle Smith</h3>
-                                        <p>
-                                            “We had the pleasure of working with Stack Delta recently. They do an amazing job to give us qualified candidates for our temporary and permanent staffing needs. They take excellent thought of the abilities and capabilities we are searching for and assist us with filling our situations as soon possible. I wouldn't hold back to suggest Stack Delta for temporary and temporary to permanent employment needs.”
-                                        </p>
+                                    <div className="isd">
+                                        <div className="carousel-caption">
+                                            <h3 className='mb-3'> Michelle Smith</h3>
+                                            <p>
+                                                “We had the pleasure of working with Stack Delta recently. They do an amazing job to give us qualified candidates for our temporary and permanent staffing needs. They take excellent thought of the abilities and capabilities we are searching for and assist us with filling our situations as soon possible. I wouldn't hold back to suggest Stack Delta for temporary and temporary to permanent employment needs.”
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
